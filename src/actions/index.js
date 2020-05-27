bc21b39537518319036db7813a70c780e18fe21a
Laure C <compointlaure@gmail.com>
2020-05-27 17:05:25 +0200

    netoyer code - enlever code en trop

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,5 @@
 export const Types = {
     SUBMIT_TODO: 'SUBMIT_TODO',
-
-
 }
 export const Actions = {
     setTodos: (results) => ({
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,21 +2,6 @@ import { combineReducers } from 'redux';
 import todo from "./todo"
 
 
-
-/**
- * combineReducers() permet de combiner les différents reducers, ce qui est plus pratique pour organiser les reducers.
- *
- * Dans notre cas le state général de l'application aura cette forme
- * {
- *   listings : {
- *     experiences: [....]
- *   },
- *   app: {
- *     isLoading: true
- *   }
- * }
- */
-
 export default combineReducers({
     todo,
 
